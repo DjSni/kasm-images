108b1b43edb62fe44ddf94341f518fd2d41d2a24
DjSni <31524614+DjSni@users.noreply.github.com>
2025-09-24 05:38:59 +0000

    Add ubuntu desktop image with customizations * Add 1Password, Brave, Filezilla, OBS, OnlyOffice, Signal, Steam, VLC and VS Code

---
+++ src/install/ubuntu/1password/install_1password.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -ex
+
+ARCH=$(arch | sed 's/aarch64/arm64/g' | sed 's/x86_64/amd64/g')
+
+if [ "${ARCH}" == "arm64" ] ; then
+    echo "OBS for arm64 currently not supported, skipping install"
+    exit 0
+fi
+
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg 
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+apt update && sudo apt -y install 1password
